5af570ece7af5b63e9b2f30dfa4e2dcb1ac81f5e
ThanhDong <cntt23022019@gmail.com>
2024-07-04 16:41:50 +0700

    push full code

---
+++ template/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">NHÀ SÁCH CHI NHÁNH HÀ NỘI</a>
+        </div>
+
+        <!--/.navbar-collapse -->
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <!-- link to publiser_list.php -->
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Publisher</a></li>
+              <!-- link to books.php -->
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <!-- link to contacts.php -->
+              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+              <!-- link to shopping cart -->
+              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <?php
+      if(isset($title) && $title == "Index") {
+    ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to online book Thành Đông</h1>
+        <p class="lead">Cơ sở 133 Nguyễn Trãi ,Thanh Xuân Hà Nội</p>
+        <p>Không có gì là không thể ! Với 1 người luôn biết cố gắng <br>
+          Với một người không bao giờ từ bỏ mục tiêu của mình ? <br>
+          Thất bại chỉ là một điểm dừng!!.Để họ nhìn lại những gì đã làm ? <br>
+          * Rút ra bài học và chuẩn bị cho sự tiếp tục tuyệt vời hơn * <br>
+
+        </p>
+      </div>
+    </div>
+    <?php } ?>
+
+    <div class="container" id="main">
